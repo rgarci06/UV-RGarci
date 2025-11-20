4f2c44add980f2543e82c7f963b5474eeaee1031
rgarci06 <rgarci@insdanielblanxart.cat>
2025-11-20 20:17:12 +0100

    GOD

---
+++ PRACTICA_1/projecte/setmana/urls.py
@@ -0,0 +1,20 @@
+'''
+Fitxer que creem nosaltres per posar les vistes de l'app
+'''
+# per poder crear l'annex d'unió url=>view
+from django.urls import path
+from . import views
+
+urlpatterns=[
+
+# path=>serveix per definir una ruta (URL) 
+# i indicar quina vista (view) s’ha d’executar quan algú hi accedeixi.
+# nom funció
+# name=>fixem nom per definir ruta i si fem canvi url només cal canviar-ho aquí
+
+#'' indica arrel, el primer argument diu quin camí després del prefix de l’app activarà aquesta vista
+  path('', views.home, name='home'),
+  path('1', views.uno, name='uno'),
+
+
+]
